ca48afdbfa169b89c585e0bcbb8b132ac592571e
Erin Tustin <erintustin@gmail.com>
2022-11-21 22:13:25 -0500

    Update config

--- cya-server/config.js
+++ cya-server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/CYA',
+    'mongoUrl': 'mongodb://localhost:27017/',
     'facebook': {
         clientId: '520628632810604',
         clientSecret: 'e3210c3d1fd1478e0d13fe5240020d06'
